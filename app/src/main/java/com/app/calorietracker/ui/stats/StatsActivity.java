5f341599fc2271e8a3ad1c10ddf3d27a61076e55
AntonBread <antonbezrukov02@protonmail.com>
2024-01-22 17:21:20 +0700

    feat: StatsActivity now displays text stats

--- app/src/main/java/com/app/calorietracker/ui/stats/StatsActivity.java
+++ app/src/main/java/com/app/calorietracker/ui/stats/StatsActivity.java
@@ -1,18 +1,29 @@
 package com.app.calorietracker.ui.stats;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import com.app.calorietracker.R;
+import com.app.calorietracker.database.AppDatabase;
+import com.app.calorietracker.database.user.UserDiaryEntity;
 import com.app.calorietracker.ui.main.MainActivity;
 import com.app.calorietracker.ui.settings.SettingsActivity;
+import com.app.calorietracker.ui.stats.StatsCalculator.WeightChangeSpeedInterval;
+import com.app.calorietracker.ui.stats.views.StatTextView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class StatsActivity extends AppCompatActivity {
     
@@ -29,13 +40,35 @@ public class StatsActivity extends AppCompatActivity {
         ALL
     }
     
+    private boolean mTimeRangeChanged;
+    
     private StatType mStatType;
     private TimeRange mTimeRange;
+    
+    private List<UserDiaryEntity> dataList;
+    
+    private StatsCalculator calculator;
+    private StatsStringFormatter stringFormatter;
+    
+    private StatTextView weightDeltaView;
+    private StatTextView weightSpeedView;
+    private StatTextView weightBmiView;
+    private StatTextView caloriesTotalView;
+    private StatTextView caloriesAvgView;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+        
+        weightDeltaView = findViewById(R.id.stats_text_weight_delta);
+        weightSpeedView = findViewById(R.id.stats_text_weight_speed);
+        weightBmiView = findViewById(R.id.stats_text_weight_bmi);
+        caloriesTotalView = findViewById(R.id.stats_text_calories_total);
+        caloriesAvgView = findViewById(R.id.stats_text_calories_avg);
+        
+        calculator = new StatsCalculator(this);
+        stringFormatter = new StatsStringFormatter(this);
         
         initTypeSelector();
         initTimeSelector();
@@ -96,6 +129,7 @@ public class StatsActivity extends AppCompatActivity {
             else {
                 mTimeRange = TimeRange.WEEK;
             }
+            mTimeRangeChanged = true;
             updateDisplayedStats();
         });
         ((RadioButton) findViewById(R.id.stats_time_week)).setChecked(true);
@@ -105,6 +139,149 @@ public class StatsActivity extends AppCompatActivity {
         if (mStatType == null || mTimeRange == null) {
             return;
         }
-        Log.d("DEBUG", "UPDATED");
+        if (mTimeRangeChanged) {
+            dataList = fetchDiaryData();
+            if (dataList == null || dataList.size() == 0) {
+                displayDataListEmptyInfo();
+                return;
+            }
+            mTimeRangeChanged = false;
+        }
+        updateStatsChart();
+        updateStatsText();
+    }
+    
+    @Nullable
+    private List<UserDiaryEntity> fetchDiaryData() {
+        LocalDate dateEnd = LocalDate.now();
+        LocalDate dateStart;
+        switch (mTimeRange) {
+            case WEEK:
+            default:
+                dateStart = dateEnd.minusWeeks(1);
+                break;
+            case MONTH:
+                dateStart = dateEnd.minusMonths(1);
+                break;
+            case MONTH_3:
+                dateStart = dateEnd.minusMonths(3);
+                break;
+            case YEAR:
+                dateStart = dateEnd.minusYears(1);
+                break;
+            case ALL:
+                dateStart = LocalDate.MIN;
+        }
+        try {
+            return AppDatabase.getInstance().userDiaryDao().getDiaryEntriesRange(dateStart, dateEnd).get();
+        }
+        catch (ExecutionException | InterruptedException e) {
+            return null;
+        }
+    }
+    
+    private void updateStatsChart() {
+        // TODO: fix method stub
+    }
+    
+    private void updateStatsText() {
+        updateTextViewsVisibility();
+        if (mStatType == StatType.CALORIES) {
+            updateCaloriesTextViews();
+        }
+        else {
+            updateWeightTextViews();
+        }
+    }
+    
+    private void updateTextViewsVisibility() {
+        if (mStatType == StatType.CALORIES) {
+            weightDeltaView.setVisibility(View.GONE);
+            weightSpeedView.setVisibility(View.GONE);
+            weightBmiView.setVisibility(View.GONE);
+            
+            caloriesTotalView.setVisibility(View.VISIBLE);
+            caloriesAvgView.setVisibility(View.VISIBLE);
+        }
+        else {
+            caloriesTotalView.setVisibility(View.GONE);
+            caloriesAvgView.setVisibility(View.GONE);
+            
+            weightDeltaView.setVisibility(View.VISIBLE);
+            weightSpeedView.setVisibility(View.VISIBLE);
+            weightBmiView.setVisibility(View.VISIBLE);
+        }
+    }
+    
+    private void updateCaloriesTextViews() {
+        int caloriesTotal = calculator.totalCalories(dataList);
+        int caloriesAvg = calculator.averageCalories(dataList, caloriesTotal);
+        
+        String caloriesTotalText = stringFormatter.getCaloriesTotalString(caloriesTotal);
+        String caloriesAvgText = stringFormatter.getCaloriesAvgString(caloriesAvg);
+        
+        caloriesTotalView.setValueText(caloriesTotalText);
+        caloriesAvgView.setValueText(caloriesAvgText);
+    }
+    
+    private void updateWeightTextViews() {
+        float delta = calculator.weightDelta(dataList);
+        WeightChangeSpeedInterval interval = calculator.changeSpeedInterval();
+        float changeSpeed = calculator.weightChangeSpeed(delta, interval);
+        float bmi = calculator.currentBodyMassIndex(dataList);
+        
+        updateWeightDeltaTextView(delta);
+        updateWeightChangeSpeedTextView(changeSpeed, interval);
+        updateWeightBMITextView(bmi);
+    }
+    
+    private void updateWeightDeltaTextView(float delta) {
+        String deltaText = stringFormatter.getWeightDeltaString(delta);
+        String deltaTitle = stringFormatter.getWeightDeltaTitle(delta);
+        Drawable deltaIcon = delta > 0 ?
+                AppCompatResources.getDrawable(this, R.drawable.ic_weight_delta_gain) :
+                AppCompatResources.getDrawable(this, R.drawable.ic_weight_delta_loss);
+        
+        if (deltaText == null) {
+            weightDeltaView.setVisibility(View.GONE);
+            return;
+        }
+        
+        weightDeltaView.setTitleText(deltaTitle);
+        
+        if (delta == 0) {
+            weightDeltaView.setValueVisibility(false);
+            return;
+        }
+        
+        weightDeltaView.setValueVisibility(true);
+        weightDeltaView.setValueText(deltaText);
+        weightDeltaView.setIcon(deltaIcon);
+    }
+    
+    private void updateWeightChangeSpeedTextView(float changeSpeed, WeightChangeSpeedInterval interval) {
+        String changeSpeedText = stringFormatter.getWeightChangeSpeedString(changeSpeed, interval);
+        
+        if (changeSpeedText == null) {
+            weightSpeedView.setVisibility(View.GONE);
+            return;
+        }
+        
+        weightSpeedView.setValueText(changeSpeedText);
+    }
+    
+    private void updateWeightBMITextView(float bmi) {
+        String bmiText = stringFormatter.getBMIString(bmi);
+        
+        if (bmiText == null) {
+            weightBmiView.setVisibility(View.GONE);
+            return;
+        }
+        
+        weightBmiView.setValueText(bmiText);
+    }
+    
+    private void displayDataListEmptyInfo() {
+        // TODO: fix method stub
     }
 }
---
+++ app/src/main/java/com/app/calorietracker/ui/stats/StatsStringFormatter.java
@@ -0,0 +1,74 @@
+package com.app.calorietracker.ui.stats;
+
+import android.content.Context;
+
+import androidx.annotation.Nullable;
+
+import com.app.calorietracker.R;
+import com.app.calorietracker.ui.stats.StatsCalculator.WeightChangeSpeedInterval;
+
+import java.util.Locale;
+
+public class StatsStringFormatter {
+    
+    private final Context context;
+    
+    public StatsStringFormatter(Context context) {
+        this.context = context;
+    }
+    
+    public String getCaloriesTotalString(int total) {
+        return context.getString(R.string.stats_calories_value_format, total);
+    }
+    
+    // Different methods with same body for potential changes in the future
+    public String getCaloriesAvgString(int avg) {
+        return context.getString(R.string.stats_calories_value_format, avg);
+    }
+    
+    @Nullable
+    public String getWeightDeltaString(float delta) {
+        if (delta == Float.MIN_VALUE) {
+            return null;
+        }
+        String fmt = context.getString(R.string.stats_weight_value_delta_format);
+        return String.format(Locale.US, fmt, Math.abs(delta));
+    }
+    
+    public String getWeightDeltaTitle(float delta) {
+        if (delta > 0) {
+            return context.getString(R.string.stats_weight_title_delta_gain);
+        }
+        else if (delta < 0) {
+            return context.getString(R.string.stats_weight_title_delta_loss);
+        }
+        else {
+            return context.getString(R.string.stats_weight_title_delta_no);
+        }
+    }
+    
+    @Nullable
+    public String getWeightChangeSpeedString(float changeSpeed, WeightChangeSpeedInterval interval) {
+        if (changeSpeed == Float.MIN_VALUE || changeSpeed == 0 || interval == WeightChangeSpeedInterval.NONE) {
+            return null;
+        }
+        String fmt = context.getString(R.string.stats_weight_value_speed_format);
+        String s = String.format(Locale.US, fmt, changeSpeed);
+        switch (interval) {
+            case WEEK:
+                s += context.getString(R.string.stats_weight_value_speed_suffix_week);
+                break;
+            case MONTH:
+                s += context.getString(R.string.stats_weight_value_speed_suffix_month);
+        }
+        return s;
+    }
+    
+    @Nullable
+    public String getBMIString(float bmi) {
+        if (bmi == Float.MIN_VALUE) {
+            return null;
+        }
+        return String.format(Locale.US, "%.1f", bmi);
+    }
+}
